d03fb9a9f9e13fd5e54e687f40f1641aad125e84
Alejandro Salamanca <alexsalamanca94@hotmail.com>
2019-09-27 11:24:17 -0700

    Added camera rotation while holding the right button of the mouse

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    public float pitch = 0;
+    public float yaw = 0;
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = new Vector3(player.transform.position.x + 0.5f, player.transform.position.y + 5.0f, player.transform.position.z - 5.0f);
+
+        if (Input.GetMouseButton(1))
+        {
+            SetRotation();
+        }
+        
+	}
+
+    private void SetRotation()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch += speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,39 +6,17 @@ public class Movement : MonoBehaviour {
 
     public float speed = 10.0f;
     public float rotationSpeed = 1.0f;
-    public Camera playerCamera;
+    
 
-    void Start()
-    {
-        playerCamera = Camera.main;
-    }
-	
 	// Update is called once per frame
 	void Update () {
         GetMovement();
         //TODO: Make the offset variables
-        playerCamera.transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y + 5.0f, transform.position.z - 5.0f);
+        
 	}
     //TODO: Improve Movement
     private void GetMovement()
     {
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    transform.Translate(speed * Time.deltaTime, 0f, 0f);
-        //}
-        //else if (Input.GetKey(KeyCode.S))
-        //{
-        //    transform.Translate(-speed * Time.deltaTime, 0f, 0f);
-        //}
-        //else if (Input.GetKey(KeyCode.A))
-        //{
-        //    transform.Translate(0f, 0f, -speed * Time.deltaTime);
-        //}
-        //else if (Input.GetKey(KeyCode.D))
-        //{
-        //    transform.Translate(0f, 0f, speed * Time.deltaTime);
-        //}
-
         transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed);
     }
 }
